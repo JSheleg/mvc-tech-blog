4c74b162d4310a2c6ca1f46599697cffd9df68ee
JSheleg <jessica.sheleg@gmail.com>
2021-08-12 17:18:50 -0500

    post model and routes set up with associations

--- models/index.js
+++ models/index.js
@@ -1,4 +1,19 @@
 const User = require('./User');
 const Post = require("./Post");
 
+//define associations
+
+
+//User can make many posts
+User.hasMany(Post, {
+    foreignKey: 'user_id'
+});
+
+
+//A post only belongs to a single user, reverse association
+Post.belongsTo(User, {
+    foreignKey: 'user_id',
+});
+
+
 module.exports = { User, Post };
