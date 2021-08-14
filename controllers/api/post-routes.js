e0a8fefd977329fa2acf1aaa2813a1c26e5950a1
JSheleg <jessica.sheleg@gmail.com>
2021-08-13 19:52:00 -0500

    seeding data

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -43,7 +43,7 @@ router.get('/:id', (req, res) => {
       where: {
         id: req.params.id
       },
-      attributes: ['id', 'post_url', 'title', 'created_at'],
+      attributes: ['id', 'post_text', 'title', 'created_at'],
       include: [
         {
           model: User,
@@ -66,10 +66,10 @@ router.get('/:id', (req, res) => {
 
 //create a post
 router.post('/', (req, res) => {
-    // expects {title: 'Taskmaster goes public!', post_url: 'https://taskmaster.com/press', user_id: 1}
+    
     Post.create({
       title: req.body.title,
-      post_url: req.body.post_url,
+      post_text: req.body.post_text,
       user_id: req.body.user_id
     })
     .then(dbPostData => res.json(dbPostData))
--- seeds/comment-seeds.js
+++ seeds/comment-seeds.js
@@ -11,17 +11,17 @@ const commentData = [{
         post_id: 2
     },
     {
-        comment_text: "sed do eiusmod tempor incididunt ut labore et dolore magna aliqua",
+        comment_text: "sed do eiusmod tempor incididunt",
         user_id: 3,
         post_id: 3
     },
     {
-        comment_text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia",
+        comment_text: "Lorem ipsum dolor sit amet consectetur",
         user_id: 4,
         post_id: 4        
     },
     {
-        comment_text: "molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum",
+        comment_text: "molestiae quas vel sint commodi repudiandae",
         user_id: 5,
         post_id: 5
     }
