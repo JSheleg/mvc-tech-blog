78b95ffefd54c3381927c8799955928b0c1b317b
JSheleg <jessica.sheleg@gmail.com>
2021-08-15 18:13:56 -0500

    sequelize

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,15 @@
 const Sequelize = require('sequelize');
+// const http = require('http')
 
 require('dotenv').config();
 
 let sequelize;
-
+// checks if env is Heroku, if so, sets sequelize to utilize the database hosted on heroku
 if(process.env.JAWSDB_URL){
-    sequelize = new Sequelize(process.env.JAWSDB_URL);
+    sequelize = new Sequelize(process.env.JAWSDB_URL, {
+        dialect: 'postgres',
+        protocol: 'postgres'
+    });
 }else{
     sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
         host: 'localhost',
@@ -14,14 +18,14 @@ if(process.env.JAWSDB_URL){
     });
 }
 
-// Make connection.
-sequelize.connect(function(err) {
-    if (err) {
-      console.error("error connecting: " + err.stack);
-      return;
-    }
-    console.log("connected as id " + connection.threadId);
-});
+// // Make connection.
+// sequelize.connect(function(err) {
+//     if (err) {
+//       console.error("error connecting: " + err.stack);
+//       return;
+//     }
+//     console.log("connected as id " + connection.threadId);
+// });
 
 
-module.exports = sequelize;
+module.exports = Sequelize;
